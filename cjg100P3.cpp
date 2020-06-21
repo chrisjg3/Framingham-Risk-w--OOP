a2dddab609ad5d829a231b85a3e8d355adc83398
Chris Joseph Gallo <chrisjg3@users.noreply.github.com>
2020-06-20 23:44:51 -0400

    Basic Set-Up by copying CS Work for Procedural Program

---
+++ cjg100P3.cpp
@@ -0,0 +1,44 @@
+
+/*
+* cjg100P3.cpp
+*
+* COSC 051 2020
+*
+*
+* Due on: June 25th, 2020
+* Author: Christopher Joseph Gallo
+*
+*
+* In accordance with the class policies and Georgetown's
+* Honor Code, I certify that, with the exception of the
+* class resources and those items noted below, I have neither
+* given nor received any assistance on this project.
+*
+* References not otherwise commented within the program source code.
+* Note that you should not mention any help from the TAs, the professor,
+* or any code taken.
+*/
+
+// Import Section
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <istream>
+#include <cmath>
+
+using namespace std;
+
+// Function Prototypes:
+
+
+
+// Beginning of Main Function:
+int main()
+{
+
+
+
+    return 0;
+}
