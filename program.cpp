a6d6eed28e2a61b8aac833cd375d2fdd30df59c6
Chris Joseph Gallo <62271962+chrisjg3@users.noreply.github.com>
2020-06-28 20:51:12 -0400

    Quick error with commented code

--- program.cpp
+++ program.cpp
@@ -1,23 +1,3 @@
-/*
-* cjg100P3.cpp
-*
-* COSC 051 2020
-* Project
-*
-* Due on: June 26th, 2020
-* Author: Christopher Joseph Gallo
-*
-*
-* In accordance with the class policies and Georgetown's
-* Honor Code, I certify that, with the exception of the
-* class resources and those items noted below, I have neither
-* given nor received any assistance on this project.
-*
-* References not otherwise commented within the program source code.
-* Note that you should not mention any help from the TAs, the professor,
-* or any code taken from the class textbooks.
-*/
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
